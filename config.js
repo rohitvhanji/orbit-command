037a52f29a42e108e1b94cc9c0e3f4da6f4f54b9
rohitvhanji <56556783+rohitvhanji@users.noreply.github.com>
2026-01-18 23:37:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 const CONFIG = {
     SUPABASE_URL: 'https://xcdnyheqtagizlcyzuzb.supabase.co',
     SUPABASE_KEY: 'sb_publishable_5cH3NNSKN1knnkm6ZMvggw_6RQdq9Nh',
-    TIMEZONEDB_KEY: 'VW4CCUCGOI2M'
+    TIMEZONEDB_KEY: 'VW4CCUCGOI2M',
 
     EMAILJS_SERVICE_ID: 'service_n2o9afl',
     EMAILJS_TEMPLATE_ID: 'template_rfv0exk',
     EMAILJS_PUBLIC_KEY: 'hK0mkYs1jfMqYzTCM'
+
 };
